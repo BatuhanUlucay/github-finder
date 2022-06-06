5a597383a1d2be1166102c88fcab3e8839399afc
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-06 17:03:48 +0300

    Simple About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+      <>
+    <h1 className='text-6xl mb-4'>Github Finder</h1>
+    <p className='text-lg text-gray-400'>Lorem Ipsum</p>
+    </>
+  )
+}
+
+export default About
