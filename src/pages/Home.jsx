1ec3aeeaf672e67230bf8bfcf068151c66e71099
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-06 17:31:56 +0300

    user list is added.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import UserResults from "../components/users/UserResults";
 
 function Home() {
   return (
-    <h1 className='text-6xl'>Welcome</h1>
-  )
+    <>
+      <UserResults />
+    </>
+  );
 }
 
-export default Home
+export default Home;
