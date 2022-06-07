d14e16eb4e380bd7b6ed4cb5c96bc56e6b10d85d
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-07 19:28:27 +0300

    refactor

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -2,7 +2,6 @@ import { createContext, useReducer } from "react";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
-const GITHUB_URL = "https://api.github.com";
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
@@ -14,94 +13,11 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
-
-    const { items } = await response.json();
-    console.log(items);
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-
-  //get a single user
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
-
-    if (response.status === 404) {
-      window.location = "/notfound";
-    } else {
-      const data = await response.json();
-      console.log(data);
-      dispatch({
-        type: "GET_USER",
-        payload: data,
-      });
-    }
-  };
-
-  //get user repos
-  const getUserRepos = async (login) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10
-    });
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
-
-    const data = await response.json();
-    console.log(data);
-    dispatch({
-      type: "GET_REPOS",
-      payload: data,
-    });
-  };
-
-
-  //clear users from state
-  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
-
-  //set loading
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        repos: state.repos,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos,
+        ...state,
+        dispatch,
       }}
     >
       {children}
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,16 +6,24 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
-function User({}) {
-  const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext);
+function User() {
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({type:"SET_LOADING"});
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({type: "GET_USER_AND_REPOS", payload: userData} );
+
+    }
+
+    getUserData();
+
+  }, [dispatch, params.login]);
 
   const {
     name,
@@ -119,52 +127,44 @@ function User({}) {
           </div>
         </div>
         <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                    <FaUsers className="text-3xl md:text-5xl"/>
-                </div>
-                <div className="stat-title pr-5">
-                    Followers
-                </div>
-                <div className="stat-value pr-5 text-3xl md:text-4xl">
-                    {followers}
-                </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaUsers className="text-3xl md:text-5xl" />
             </div>
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                    <FaUserFriends className="text-3xl md:text-5xl"/>
-                </div>
-                <div className="stat-title pr-5">
-                    Following
-                </div>
-                <div className="stat-value pr-5 text-3xl md:text-4xl">
-                    {following}
-                </div>
+            <div className="stat-title pr-5">Followers</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {followers}
             </div>
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                    <FaCodepen className="text-3xl md:text-5xl"/>
-                </div>
-                <div className="stat-title pr-5">
-                    Public Repos
-                </div>
-                <div className="stat-value pr-5 text-3xl md:text-4xl">
-                    {public_repos}
-                </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaUserFriends className="text-3xl md:text-5xl" />
             </div>
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                    <FaStore className="text-3xl md:text-5xl"/>
-                </div>
-                <div className="stat-title pr-5">
-                    Public Gists
-                </div>
-                <div className="stat-value pr-5 text-3xl md:text-4xl">
-                    {public_gists}
-                </div>
+            <div className="stat-title pr-5">Following</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {following}
             </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaCodepen className="text-3xl md:text-5xl" />
+            </div>
+            <div className="stat-title pr-5">Public Repos</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {public_repos}
+            </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaStore className="text-3xl md:text-5xl" />
+            </div>
+            <div className="stat-title pr-5">Public Gists</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {public_gists}
+            </div>
+          </div>
         </div>
-        <RepoList repos={repos}/>
+        <RepoList repos={repos} />
       </div>
     </>
   );
